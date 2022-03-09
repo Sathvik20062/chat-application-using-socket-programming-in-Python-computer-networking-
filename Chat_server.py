e093cffaeee4a66b4599c48035bc7aa25d9f9cbf
Sirigiri Naga Pavan Sathvik Reddy <84168199+Sathvik20062@users.noreply.github.com>
2022-03-09 16:45:50 +0530

    Add files via upload

---
+++ Chat_server.py
@@ -0,0 +1,53 @@
+from threading import Thread
+import socket
+
+def accept_incoming_connections():
+    while True:
+        client, client_address = SERVER.accept()
+        print("%s:%s has connected." % client_address)
+        client.send(bytes("Hello user! Please type your name and press enter.", "utf8"))
+        addresses[client] = client_address
+        Thread(target=handle_client, args=(client,)).start()
+
+
+def handle_client(client):
+    name = client.recv(BUFSIZ).decode("utf8") 
+    welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit.' % name
+    client.send(bytes(welcome, "utf8"))
+    msg = "%s has joined the chat!" % name
+    broadcast(bytes(msg, "utf8"))
+    clients[client] = name
+
+    while True:
+        msg = client.recv(BUFSIZ)
+        if msg != bytes("{quit}", "utf8"):
+            broadcast(msg, name+": ")
+        else:
+            client.send(bytes("{quit}", "utf8"))
+            client.close()
+            del clients[client]
+            broadcast(bytes("%s has left the chat." % name, "utf8"))
+            break
+
+
+def broadcast(msg, prefix=""): 
+
+    for sock in clients:
+        sock.send(bytes(prefix, "utf8")+msg)
+
+clients = {}
+addresses = {}
+HOST = socket.gethostname()
+PORT = 8888
+BUFSIZ = 1024 
+ADDR = (HOST, PORT)
+SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+SERVER.bind(ADDR) 
+
+if __name__ == "__main__":
+    SERVER.listen(5)
+    print("Waiting for connection...")
+    ACCEPT_THREAD = Thread(target=accept_incoming_connections)
+    ACCEPT_THREAD.start()
+    ACCEPT_THREAD.join()
+    SERVER.close()
